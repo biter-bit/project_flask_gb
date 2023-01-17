3f0d23c4c1b239855413357e2b3afdd5dfba58f2
michael <kurashevmichael@gmail.com>
2023-01-17 22:47:32 +0300

    lesson_2-update

--- blog/app.py
+++ blog/app.py
@@ -3,12 +3,12 @@ from blog.users.views import user_app
 from blog.articles.views import article_app
 
 
+def register_blueprints(app: Flask):
+    app.register_blueprint(user_app)
+    app.register_blueprint(article_app)
+
+
 def create_app() -> Flask:
     app = Flask(__name__)
     register_blueprints(app)
     return app
-
-
-def register_blueprints(app: Flask):
-    app.register_blueprint(user_app)
-    app.register_blueprint(article_app)
